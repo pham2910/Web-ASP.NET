8c8fb854c25a3d35f524a5d17ff1c14782332846
Quynh <phamthiquynh.lamdong@gmail.com>
2021-10-16 16:10:59 +0700

    sql file

---
+++ Models/Database/SQLQuery1.sql
@@ -0,0 +1,57 @@
+﻿create database ReviewWeb
+go
+use ReviewWeb
+go
+
+create table Users (
+	ID int IDENTITY(1,1) PRIMARY KEY,
+	UserName nvarchar(50),
+	Email nvarchar(100),
+	Pwd varchar(100)
+);
+
+create table Admin (
+	AdminName nvarchar(50) NOT NULL,
+	Pwd varchar(50)
+);
+
+create table Company (
+	Id int IDENTITY(1,1) PRIMARY KEY,
+	Name nvarchar(MAX),
+	Address nvarchar(MAX),
+	Logo nvarchar(max),
+	Web varchar(100),
+	Rating float,
+	UserRequest int,
+	Confirm bit,
+	FieldID int,
+);
+
+create table Review (
+	ReviewId int IDENTITY(1,1) PRIMARY KEY,
+	UserId int,
+	ComId int,
+	DateCreated Date,
+	Rating int,
+	Content nvarchar(max),
+	Incognito bit,
+)
+
+create table Field (
+	ID int IDENTITY(1, 1) PRIMARY KEY,
+	FieldName nvarchar(100),
+)
+
+
+insert into Admin values ('admin', '123456')
+
+insert into Users values ('Quynh', 'quynh@gmail.com', '123456')
+insert into Users values ('Sang', 'sang@gmail.com', '123456')
+insert into Users values ('Hung', 'hung@gmail.com', '123456')
+
+insert into Field values ('Dịch vụ')
+insert into Field values ('Phần mềm')
+insert into Field values ('Bất động sản')
+
+
+
